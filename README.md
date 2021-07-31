cd0d2e7564ccd6ddf425b5690196f8bf9c2c2469
Ruzli96 <ruzli1996@gmail.com>
2021-07-31 19:50:20 -0400

    Project 2 code and README update

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # 100_Python_Projects
 A few minor python projects 
 
-### Project 1
+#### Project 1
 Generates a band name based on the city you grew up in and your pet name
+
+#### Project 2
+Calculation of tip and amount being paid per person
---
+++ project_2.py
@@ -0,0 +1,10 @@
+# Calculates and displays the total amount + tip paid per person
+
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the total of your bill? \n$"))
+print(type(total_bill))
+split_number = float(input("How many people would you like this bill to be split among?\n"))
+tip = float(input("What is your tip percentage? 10, 12, or 15?\n"))
+amount_per = total_bill/split_number
+tip_amount_per = (amount_per*(tip/100)) + amount_per
+print(f'Each person should pay\n{round(tip_amount_per, 2)}')
