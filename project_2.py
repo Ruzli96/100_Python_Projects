a73ef7efeb3ebcd4a14cca1eb52bab317df45617
Ruzli96 <ruzli1996@gmail.com>
2021-08-01 12:34:52 -0400

    Added 2 decimal place formatting

--- project_2.py
+++ project_2.py
@@ -6,4 +6,4 @@ split_number = int(input("How many people would you like this bill to be split a
 tip = int(input("What is your tip percentage? 10, 12, or 15?\n"))
 amount_per = total_bill/split_number
 tip_amount_per = (amount_per*(tip/100)) + amount_per
-print(f'Each person should pay\n${round(tip_amount_per, 2)}')
+print(f'Each person should pay\n${"{:.2f}".format(round(tip_amount_per, 2))}')
